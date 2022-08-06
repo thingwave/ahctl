4dfecf8d482c219e180946e37527cd565518aacc
Jens Eliasson <jens.eliasson@gmail.com>
2022-08-06 13:25:42 +0200

    added support for more features

--- src/main/datamodels.go
+++ src/main/datamodels.go
@@ -1,29 +1,82 @@
 package main
 
-
 type SystemList struct {
-  Data []System 
-  Count int
+	Data  []System
+	Count int
 }
 
 type System struct {
-    Id int
-    SystemName string
-    Address string
-    Port int
-    AuthenticationInfo string
-    CreatedAt string
-    UpdatedAt string
+	Id                 int
+	SystemName         string
+	Address            string
+	Port               int
+	AuthenticationInfo string
+	CreatedAt          string
+	UpdatedAt          string
 }
 
 type ServiceDefinitionList struct {
-  Data []ServiceDefinition `json:"data"`
-  Count int `json:"count"`
+	Data  []ServiceDefinition `json:"data"`
+	Count int                 `json:"count"`
 }
 
 type ServiceDefinition struct {
-  Id int `json:"id"`
-  ServiceDefinition string `json:"serviceDefinition"`
-  CreatedAt string `json:"createdAt"`
-  UpdatedAt string `json:"updatedAt"`
+	Id                int    `json:"id"`
+	ServiceDefinition string `json:"serviceDefinition"`
+	CreatedAt         string `json:"createdAt"`
+	UpdatedAt         string `json:"updatedAt"`
+}
+
+type ServiceQueryRequest struct {
+	InterfaceRequirements        []string           `json:"interfaceRequirements"`
+	MaxVersionRequirement        *int               `json:"maxVersionRequirement,omitempty"`
+	MinVersionRequirement        *int               `json:"minVersionRequirement,omitempty"`
+	MetadataRequirements         *map[string]string `json:"MetadataRequirements,omitempty"`
+	PingProviders                *bool              `json:"pingProviders,omitempty"`
+	SecurityRequirements         []string           `json:"securityRequirements,omitempty"`
+	ServiceDefinitionRequirement string             `json:"serviceDefinitionRequirement"`
+	VersionRequirement           *int               `json:"versionRequirement,omitempty"`
+}
+
+type ServiceQueryResponse struct {
+	ServiceQueryData []ServiceQueryEntry `json:"serviceQueryData"`
+	UnfilteredHits   int                 `json:"unfilteredHits"`
+}
+
+type ServiceQueryEntry struct {
+	Id            int64              `json:"id"`
+	EndOfValidity *string            `json:"endOfValidity,omitempty"`
+	Interfaces    []InterfaceEntry   `json:"interfaces"`
+	Metadata      *map[string]string `json:"metadata,omitempty"`
+	Provider      ProviderDTO        `json:"provider"`
+	Secure        string             `json:"secure"`
+	/*"serviceDefinition": {
+	      "createdAt": "string",
+	      "id": 0,
+	      "serviceDefinition": "string",
+	      "updatedAt": "string"
+	    },
+	],*/
+	ServiceUri string `json:"serviceUri"`
+	CreatedAt  string `json:"createdAt"`
+	UpdatedAt  string `json:"updatedAt"`
+	Version    *int   `json:"version,omitempty"`
+}
+
+type InterfaceEntry struct {
+	Id            int64  `json:"id"`
+	InterfaceName string `json:"interfaceName"`
+	CreatedAt     string `json:"createdAt"`
+	UpdatedAt     string `json:"updatedAt"`
+}
+
+type ProviderDTO struct {
+	Id                 int64              `json:"id"`
+	Address            string             `json:"address"`
+	AuthenticationInfo *string            `json:"authenticationInfo,omitempty"`
+	Metadata           *map[string]string `json:"metadata,omitempty"`
+	Port               int                `json:"port"`
+	SystemName         string             `json:"systemName"`
+	CreatedAt          string             `json:"createdAt"`
+	UpdatedAt          string             `json:"updatedAt"`
 }
--- src/main/main.go
+++ src/main/main.go
@@ -2,156 +2,195 @@
 package main
 
 import (
-  "os"
-  "log"
-  "io/ioutil"
-  "flag"
-  "fmt"
-  "time"
-  "errors"
-  "crypto/x509"
-  "crypto/tls"
-  "net/http"
-  "encoding/json"
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-
 func main() {
-  command := flag.String("cmd", "echo", "Eclipse Arrowhead command to execute")
-  targetUri := flag.String("sr", "https://127.0.0.1:8443/serviceregistry", "Service Registry URI")
-  ca := flag.String("cafile", "", "Root CA PEM file")
-  cert := flag.String("cert", "", "Client certificate PEM file")
-  key := flag.String("key", "", "Client certificate key PEM file")
-  flag.Parse()
+	command := flag.String("cmd", "sr-echo", "Eclipse Arrowhead command to execute")
+	targetUri := flag.String("sr", "https://127.0.0.1:8443/serviceregistry", "Service Registry URI")
+	ca := flag.String("cafile", "", "Root CA PEM file")
+	cert := flag.String("cert", "", "Client certificate PEM file")
+	key := flag.String("key", "", "Client certificate key PEM file")
+	verbose := flag.String("verbose", "false", "Makes client more verbose whens set to true")
+	flag.Parse()
 
-  if (*ca != "" && (*cert == "" || *key == "")) || (*cert != "" && (*ca == "" || *key == "")) || (*key != "" && (*cert == "" || *ca == ""))   {
-    fmt.Println("Error: missing arguments!")
-    fmt.Println("Use 'ahctl --help' to see usage.")
-    return
-  }
+	if (*ca != "" && (*cert == "" || *key == "")) || (*cert != "" && (*ca == "" || *key == "")) || (*key != "" && (*cert == "" || *ca == "")) {
+		fmt.Println("Error: missing arguments!")
+		fmt.Println("Use 'ahctl --help' to see usage.")
+		return
+	}
 
-  if *command == "echo" {
-  } else if *command == "get-all-systems" {
-    } else if *command == "get-all-services" {
-  } else {
-    fmt.Printf("Unknown command: %s\n", *command)
-    return
-  }
+	if *command == "sr-echo" || *command == "or-echo" || *command == "au-echo" || *command == "dm-echo" {
+	} else if *command == "get-all-systems" {
+	} else if *command == "get-all-services" {
+	} else {
+		fmt.Printf("Unknown command: %s\n", *command)
+		return
+	}
 
-  if *ca != "" && *cert != "" && *key != "" {
-    ok, _ := fileExists(*ca)
-    if !ok {
-      fmt.Printf("Error: ca file %s does not exist\n", *ca)
-      return
-    }
-    ok, _ = fileExists(*cert)
-    if !ok {
-      fmt.Printf("Error: certificate file %s does not exist\n", *cert)
-      return
-    }
-    ok, _ = fileExists(*key)
-    if !ok {
-      fmt.Printf("Error: key file %s does not exist\n", *key)
-      return
-    }
+	if *ca != "" && *cert != "" && *key != "" {
+		ok, _ := fileExists(*ca)
+		if !ok {
+			fmt.Printf("Error: ca file %s does not exist\n", *ca)
+			return
+		}
+		ok, _ = fileExists(*cert)
+		if !ok {
+			fmt.Printf("Error: certificate file %s does not exist\n", *cert)
+			return
+		}
+		ok, _ = fileExists(*key)
+		if !ok {
+			fmt.Printf("Error: key file %s does not exist\n", *key)
+			return
+		}
 
-    caCert, err := ioutil.ReadFile(*ca)
-    if err != nil {
-      log.Fatalf("Error opening cert file %s, Error: %s", *ca, err)
-    }
-    caCertPool := x509.NewCertPool()
-    caCertPool.AppendCertsFromPEM(caCert)
+		caCert, err := ioutil.ReadFile(*ca)
+		if err != nil {
+			log.Fatalf("Error opening cert file %s, Error: %s", *ca, err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
 
-    myCert, err := tls.LoadX509KeyPair(*cert, *key)
-    if err != nil {
-        log.Fatalf("Error creating x509 keypair from client cert file %s and client key file %s", *cert, *key)
-    }
+		myCert, err := tls.LoadX509KeyPair(*cert, *key)
+		if err != nil {
+			log.Fatalf("Error creating x509 keypair from client cert file %s and client key file %s", *cert, *key)
+		}
 
-    t := &http.Transport{
-      TLSClientConfig: &tls.Config{
-        Certificates: []tls.Certificate{myCert},
-        RootCAs:      caCertPool,
-        InsecureSkipVerify: false,
-      },
-    }
+		t := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates:       []tls.Certificate{myCert},
+				RootCAs:            caCertPool,
+				InsecureSkipVerify: false,
+			},
+		}
 
-    client := http.Client{Transport: t, Timeout: 10 * time.Second}
+		client := http.Client{Transport: t, Timeout: 10 * time.Second}
 
-    if *command == "echo" {
-      data, err := getData(client, *targetUri + "/echo")
-      if err == nil {
-        fmt.Println(data)
-      }
-    }
+		if *command == "echo" {
+			data, err := getData(client, *targetUri+"/echo")
+			if err == nil {
+				fmt.Println(data)
+			}
+		}
 
-  } else {
-	  fmt.Println("Running unsecure mode....\n")
-	  client := http.Client{Timeout: 10 * time.Second}
+	} else {
+		fmt.Println("Running unsecure mode....\n")
+		client := http.Client{Timeout: 10 * time.Second}
 
-    if *command == "echo" {
-  	  data, err := getData(client, *targetUri + "/echo")
-	    if err != nil {
-        fmt.Printf("Cound not connect to '%s'\n", *targetUri + "/echo")
-        return
-	    } else {
-	      fmt.Println(data)
-	    }
-    } else if *command == "get-all-systems" {
-      data, err := getData(client, *targetUri + "/mgmt/systems?direction=ASC&sort_field=id")
-	    if err == nil {
-        var response SystemList
-        json.Unmarshal([]byte(data), &response)
+		if *command == "sr-echo" { // GET to serviceregistry/echo
+			data, err := getData(client, *targetUri+"/echo")
+			if err != nil {
+				fmt.Printf("Could not connect to '%s'\n", *targetUri+"/echo")
+				return
+			} else {
+				fmt.Println(data)
+			}
+		} else if *command == "get-all-systems" {
+			data, err := getData(client, *targetUri+"/mgmt/systems?direction=ASC&sort_field=id")
+			if err == nil {
+				var response SystemList
+				json.Unmarshal([]byte(data), &response)
 
-        empJSON, _ := json.MarshalIndent(response, "", "  ")
-        fmt.Println(string(empJSON))
-	    }
-    } else if *command == "get-all-services" {
-      data, err := getData(client, *targetUri + "/mgmt/services?direction=ASC&sort_field=id")
-	    if err == nil {
-	      //fmt.Println(data)
+				empJSON, _ := json.MarshalIndent(response, "", "  ")
+				fmt.Println(string(empJSON))
+			}
+		} else if *command == "get-all-services" {
+			data, err := getData(client, *targetUri+"/mgmt/services?direction=ASC&sort_field=id")
+			if err == nil {
+				//fmt.Println(data)
 
-        var response ServiceDefinitionList
-        json.Unmarshal([]byte(data), &response)
+				var response ServiceDefinitionList
+				json.Unmarshal([]byte(data), &response)
 
-        empJSON, _ := json.MarshalIndent(response, "", "  ")
-        fmt.Println(string(empJSON))
-      }
-    }
-  }
+				empJSON, _ := json.MarshalIndent(response, "", "  ")
+				fmt.Println(string(empJSON))
+			}
+		} else if *command == "or-echo" { // GET to orchestrator/echo
+		} else if *command == "au-echo" { // GET to authorization/echo
+		} else if *command == "dm-echo" { // GET to datamanager/echo
+			var sreq ServiceQueryRequest
+			sreq.ServiceDefinitionRequirement = "proxy"
+			sreq.InterfaceRequirements = []string{"HTTP-INSECURE-JSON"}
+			var minVerReq int
+			sreq.MinVersionRequirement = &minVerReq
+			*sreq.MinVersionRequirement = 1
+
+			reqJSON, _ := json.MarshalIndent(sreq, "", "  ")
+			if *verbose != "false" {
+				fmt.Println(string(reqJSON))
+			}
+
+			req, err := http.NewRequest("POST", *targetUri+"/query", bytes.NewBuffer(reqJSON))
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("response Status: '%s'\n", resp.Status)
+			if resp.Status == "200 " {
+				//fmt.Println("response Headers:", resp.Header)
+				body, _ := ioutil.ReadAll(resp.Body)
+				if *verbose != "false" {
+					fmt.Println("response Body:", string(body))
+				}
+
+				var serviceQueryResponse ServiceQueryResponse
+				if err := json.Unmarshal(body, &serviceQueryResponse); err != nil {
+					panic(err)
+				}
+				fmt.Printf("%+v\n", serviceQueryResponse.ServiceQueryData[0])
+			}
+		}
+	}
 
 }
 
 func getData(client http.Client, uri string) (string, error) {
-  request, err := http.NewRequest(http.MethodGet, uri, nil)
-  if err != nil {
-    //log.Fatalf("request failed : %v", err)
-    return "", err
-  }
+	request, err := http.NewRequest(http.MethodGet, uri, nil)
+	if err != nil {
+		//log.Fatalf("request failed : %v", err)
+		return "", err
+	}
 
-  resp, err := client.Do(request)
-  if err != nil {
-    //log.Fatalf("request failed : %v", err)
-    return "", err
-  }
-  defer resp.Body.Close()
+	resp, err := client.Do(request)
+	if err != nil {
+		//log.Fatalf("request failed : %v", err)
+		return "", err
+	}
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err == nil {
-    return string(body), nil
-  } else {
-    fmt.Printf("final error: %v\n", err);
-    return "", err
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		return string(body), nil
+	} else {
+		fmt.Printf("final error: %v\n", err)
+		return "", err
+	}
 }
 
 func fileExists(fileName string) (bool, error) {
-    _, err := os.Stat(fileName)
-    if err == nil {
-        return true, nil
-    }
-    if errors.Is(err, os.ErrNotExist) {
-        return false, nil
-    }
-    return false, err
+	_, err := os.Stat(fileName)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
-
